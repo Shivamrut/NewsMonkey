84266e501e6182b7aab3046c7f0194203657ead0
maxus_red <incineroaraditya@gmail.com>
2024-06-05 20:30:34 +0530

    project setup done, class based components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <>
-      </>
-  );
+export default class App  extends Component {
+  render() {
+    return (
+      <div> Hello</div>
+    )
+  }
 }
-
-export default App;
