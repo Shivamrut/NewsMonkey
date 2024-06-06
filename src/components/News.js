40e5515b54dd85059343dad8bc53e04199a03f6f
maxus_red <incineroaraditya@gmail.com>
2024-06-06 15:52:30 +0530

    added prev and next button for pagination

--- src/components/News.js
+++ src/components/News.js
@@ -8,35 +8,110 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
+      pageSize: 10,
+      maxPages : 1
     };
   }
 
-  async componentDidMount(){
-    let apiUrl = 'https://newsapi.org/v2/top-headlines?country=us'
-    let data = await fetch(apiUrl,{
-      headers:{
-          "X-Api-Key":"527b2700670e493d91b09cc687055fe6"
-      }
-  })
-  let parsedData = await data.json()
-  console.log(parsedData)
-  this.setState({
-    articles : parsedData.articles
-  })
+  async componentDidMount() {
+    let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&page=${this.state.page}&pageSize=${this.state.pageSize}`;
+    let data = await fetch(apiUrl, {
+      headers: {
+        "X-Api-Key": "527b2700670e493d91b09cc687055fe6",
+      },
+    });
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      maxPages: Math.ceil( parsedData.totalResults/this.state.pageSize)
+    });
   }
 
+  handleNext = async () => {
+    let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&page=${
+      this.state.page + 1
+    }&pageSize=${this.state.pageSize}`;
+    let data = await fetch(apiUrl, {
+      headers: {
+        "X-Api-Key": "527b2700670e493d91b09cc687055fe6",
+      },
+    });
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      maxPages: Math.ceil( parsedData.totalResults/this.state.pageSize)
+    });
+  };
+  handlePrev = async () => {
+    let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&page=${
+      this.state.page - 1
+    }&pageSize=${this.state.pageSize}`;
+    let data = await fetch(apiUrl, {
+      headers: {
+        "X-Api-Key": "527b2700670e493d91b09cc687055fe6",
+      },
+    });
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      maxPages: Math.ceil( parsedData.totalResults/this.state.pageSize)
+    });
+  };
 
-  
   render() {
     return (
       <>
         <div className="container my-3 ">
           <h1>NewsMonkey - Top Headlines</h1>
 
+          <div className="container d-flex justify-content-center">
+            <nav aria-label="Page navigation example">
+              <ul className="pagination">
+                <li className="page-item">
+                  <button
+                    type="button"
+                    className="btn btn-secondary  m-3"
+                    disabled={this.state.page <= 1}
+                    onClick={this.handlePrev}
+                  >
+                    &larr; Previous
+                  </button>
+                </li>
+                {/* <li className="page-item">
+                <a className="page-link" href="/">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">
+                  3
+                </a>
+              </li> */}
+                <li className="page-item">
+                  <button
+                    type="button"
+                    className="btn btn-secondary  m-3"
+                    disabled={this.state.page >= this.state.maxPages}
+                    onClick={this.handleNext}
+                  >
+                    Next &rarr;
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
           <div className="row">
             {this.state.articles.map((e) => {
               let { title, author, description, url, urlToImage } = e;
-              // console.log(author)
               return (
                 <div className="col-md-4" key={url}>
                   <NewsItem
